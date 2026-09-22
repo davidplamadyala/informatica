e8fca65257956d870d795e83a7f2fa7250d03803
70674600 <davidplamadala@gmail.com>
2026-09-22 12:47:50 +0200

    esercizio 1 capasso

---
+++ studenti/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Studente s= new Studente ( "David",  "Parmakliy-Plamadyala", 17 ,1.83);
+        System.out.println(s.nome);
+    }
+}
---
+++ studenti/src/Studente.java
@@ -0,0 +1,13 @@
+public class Studente {
+    String nome;
+    String cognome;
+    int eta;
+    double altezza;
+
+    public Studente(String nome, String cognome, int eta, double altezza){
+        this.nome=nome;
+        this.cognome=cognome;
+        this.eta=eta;
+        this.altezza=altezza;
+    }
+}
